9bdab57c28f34419b3c7fd3b1f8a2bcebfeb14dd
mohamed kamal <mkahf5103@gmail.com>
2022-08-24 18:07:52 +0400

    add category model

---
+++ InvoiceProject/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace InvoiceProject.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+
+    }
+}
